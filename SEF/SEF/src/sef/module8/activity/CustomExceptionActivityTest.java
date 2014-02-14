ae63c55675b3da2cd1d1a513ab181541a49feae5
treinamento <treinamento@MW7D4HGCQP6CJX.dir.svc.accenture.com>
2014-02-14 10:29:57 -0300

    OK

--- SEF/SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
+++ SEF/SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
@@ -8,24 +8,49 @@ public class CustomExceptionActivityTest {
 	//1 - Type main method and call validateUser() from it.
 	//Call this method with different names to test it 
 
+	public static void main(String[] Args){
+		
+		CustomExceptionActivityTest c = new CustomExceptionActivityTest();
+		
+		try{
+		c.validateUser(" Mike");
+		
+		}catch(CustomExceptionActivity e){
+			System.out.println(e.getMessage());
+			
+		}
+		
+	}
 
-void validateUser(String name)
+void validateUser(String name) throws CustomExceptionActivity
 {
 	//array of names
-String []validUsers={"John","Mike","Shanti","Stacie"};
+	String []validUsers={"John","Mike","Shanti","Stacie"};
 //flag stores 1 if a match is found else it should remain 0 
-int flag=0;
-for(int i=0;i<4;i++)
-{
-	
+	int flag=0;
+	for(int i=0;i<4;i++){
+		
+		if(name.trim().equals(validUsers[i])){
+			flag = 1;
+		}
 //2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly 
 
 }
 //3 - check if flag is zero, throw CustomExceptionActivity Exception.
+	if(flag == 0){
+		
+		throw new CustomExceptionActivity();
+		
+	}
+	
+	else{
+		System.out.println("Welcome to Payroll program");
+	}
 //You may also have to declare this exception in the method call using throws.
-
+	
 
 //4 - else if flag is one, print a message "Welcome to Payroll program". 
-
+	
 }
-}
+	}
+	
